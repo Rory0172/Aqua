0c8c3bb3350fb71a11371bd76be80961fa0689ac
catweazle0172 <mike_de_bruin_8@hotmail.com>
2019-01-14 23:07:21 +0100

    updated gpiocontroller

--- gpiocontroller.py
+++ gpiocontroller.py
@@ -12,31 +12,31 @@ config.read('settings.ini')
 # read values from a section
 mlpersec = config.getfloat('mlpersec', 'mlpersec')
 
-doseerpompgpio1 = config.getint('gpio', 'doseerpompgpio1')
-doseerpompgpio2 = config.getint('gpio', 'doseerpompgpio2')
-doseerpompgpio3 = config.getint('gpio', 'doseerpompgpio3')
-doseerpompgpio4 = config.getint('gpio', 'doseerpompgpio4')
+pomp_1_gpio = config.getint('gpio', 'pomp_1_gpio')
+pomp_2_gpio = config.getint('gpio', 'pomp_2_gpio')
+pomp_3_gpio = config.getint('gpio', 'pomp_3_gpio')
+pomp_4_gpio = config.getint('gpio', 'pomp_4_gpio')
 
-pomp1ml = config.getint('pomp', 'pomp1ml')
-pomp2ml = config.getint('pomp', 'pomp2ml')
-pomp3ml = config.getint('pomp', 'pomp3ml')
-pomp4ml = config.getint('pomp', 'pomp4ml')
+pomp_1_volume = config.getint('pomp', 'pomp_1_volume')
+pomp_2_volume = config.getint('pomp', 'pomp_2_volume')
+pomp_3_volume = config.getint('pomp', 'pomp_3_volume')
+pomp_4_volume = config.getint('pomp', 'pomp_4_volume')
 
 scheduledtime = config.get('time', 'time')
 
-pomp1insec = pomp1ml * mlpersec
-pomp2insec = pomp2ml * mlpersec
-pomp3insec = pomp3ml * mlpersec
-pomp4insec = pomp4ml * mlpersec
+pomp1insec = pomp_1_volume * mlpersec
+pomp2insec = pomp_2_volume * mlpersec
+pomp3insec = pomp_3_volume * mlpersec
+pomp4insec = pomp_4_volume * mlpersec
 
 #Tijd omzetten om te vergelijken
 scheduled = datetime.datetime.strptime(scheduledtime, "%H:%M")
 now = datetime.datetime.now()
 
-pomp1 = config.get('pomp', 'pomp1')
-pomp2 = config.get('pomp', 'pomp2')
-pomp3 = config.get('pomp', 'pomp3')
-pomp4 = config.get('pomp', 'pomp4')
+pomp1 = config.get('pomp', 'schedule_pomp_1')
+pomp2 = config.get('pomp', 'schedule_pomp_2')
+pomp3 = config.get('pomp', 'schedule_pomp_3')
+pomp4 = config.get('pomp', 'schedule_pomp_4')
 
 weekdag = date.today().weekday()
 
@@ -44,12 +44,12 @@ if str(weekdag) in pomp1 and now.hour == scheduled.hour and now.minute == schedu
    	#Pomp1
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup(doseerpompgpio1,GPIO.OUT)
-	print "Pomp 1 aan voor %d sec om %d ml te pompen." % (pomp1insec,pomp1ml)
-	GPIO.output(doseerpompgpio1,GPIO.LOW)
+	GPIO.setup(pomp_1_gpio,GPIO.OUT)
+	print "Pomp 1 aan voor %d sec om %d ml te pompen." % (pomp1insec,pomp_1_volume)
+	GPIO.output(pomp_1_gpio,GPIO.LOW)
 	time.sleep (pomp1insec)
 	print 'Pomp 1 uit.'
-	GPIO.output(doseerpompgpio1,GPIO.HIGH)
+	GPIO.output(pomp_1_gpio,GPIO.HIGH)
 	GPIO.cleanup()
 else :
    	print ('Pomp 1 hoeft niet te pompen.')
@@ -58,12 +58,12 @@ if str(weekdag) in pomp2 and now.hour == scheduled.hour and now.minute == schedu
    	#Pomp2
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup(doseerpompgpio2,GPIO.OUT)
-	print "Pomp 2 aan voor %d sec om %d ml te pompen." % (pomp2insec,pomp2ml)
-	GPIO.output(doseerpompgpio2,GPIO.LOW)
+	GPIO.setup(pomp_2_gpio,GPIO.OUT)
+	print "Pomp 2 aan voor %d sec om %d ml te pompen." % (pomp2insec,pomp_2_volume)
+	GPIO.output(pomp_2_gpio,GPIO.LOW)
 	time.sleep (pomp2insec)
 	print 'Pomp 2 uit.'
-	GPIO.output(doseerpompgpio2,GPIO.HIGH)
+	GPIO.output(pomp_2_gpio,GPIO.HIGH)
 	GPIO.cleanup()
 else :
    	print ('Pomp 2 hoeft niet te pompen.')
@@ -72,12 +72,12 @@ if str(weekdag) in pomp3 and now.hour == scheduled.hour and now.minute == schedu
    	#Pomp3
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup(doseerpompgpio3,GPIO.OUT)
-	print "Pomp 3 aan voor %d sec om %d ml te pompen." % (pomp3insec,pomp3ml)
-	GPIO.output(doseerpompgpio3,GPIO.LOW)
+	GPIO.setup(pomp_3_gpio,GPIO.OUT)
+	print "Pomp 3 aan voor %d sec om %d ml te pompen." % (pomp3insec,pomp_3_volume)
+	GPIO.output(pomp_3_gpio,GPIO.LOW)
 	time.sleep (pomp3insec)
 	print 'Pomp 3 uit.'
-	GPIO.output(doseerpompgpio3,GPIO.HIGH)
+	GPIO.output(pomp_3_gpio,GPIO.HIGH)
 	GPIO.cleanup()
 else :
    	print ('Pomp 3 hoeft niet te pompen.')
@@ -86,12 +86,12 @@ if str(weekdag) in pomp4 and now.hour == scheduled.hour and now.minute == schedu
    	#Pomp4
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup(doseerpompgpio4,GPIO.OUT)
-	print "Pomp 4 aan voor %d sec om %d ml te pompen." % (pomp4insec,pomp4ml)
-	GPIO.output(doseerpompgpio4,GPIO.LOW)
+	GPIO.setup(pomp_4_gpio,GPIO.OUT)
+	print "Pomp 4 aan voor %d sec om %d ml te pompen." % (pomp4insec,pomp_4_volume)
+	GPIO.output(pomp_4_gpio,GPIO.LOW)
 	time.sleep (pomp4insec)
 	print 'Pomp 4 uit.'
-	GPIO.output(doseerpompgpio4,GPIO.HIGH)
+	GPIO.output(pomp_4_gpio,GPIO.HIGH)
 	GPIO.cleanup()
 else :
    	print ('Pomp 4 hoeft niet te pompen.')
