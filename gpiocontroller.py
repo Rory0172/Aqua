09453a449d0195af16196000beabb1765aa2745e
catweazle0172 <mike_de_bruin_8@hotmail.com>
2019-01-27 22:52:57 +0100

    updated test/calibrate buttons

--- gpiocontroller.py
+++ gpiocontroller.py
@@ -12,36 +12,36 @@ config.read('settings.ini')
 # read values from a section
 mlpersec = config.getfloat('mlpersec', 'mlpersec')
 
-pomp_1_gpio = config.getint('gpio', 'pomp_1_gpio')
-pomp_2_gpio = config.getint('gpio', 'pomp_2_gpio')
-pomp_3_gpio = config.getint('gpio', 'pomp_3_gpio')
-pomp_4_gpio = config.getint('gpio', 'pomp_4_gpio')
+pump_1_gpio = config.getint('gpio', 'pump_1_gpio')
+pump_2_gpio = config.getint('gpio', 'pump_2_gpio')
+pump_3_gpio = config.getint('gpio', 'pump_3_gpio')
+pump_4_gpio = config.getint('gpio', 'pump_4_gpio')
 
 reservoir_1 = config.getint('reservoir', 'reservoir1')
 reservoir_2 = config.getint('reservoir', 'reservoir2')
 reservoir_3 = config.getint('reservoir', 'reservoir3')
 reservoir_4 = config.getint('reservoir', 'reservoir4')
 
-pomp_1_volume = config.getint('pomp', 'pomp_1_volume')
-pomp_2_volume = config.getint('pomp', 'pomp_2_volume')
-pomp_3_volume = config.getint('pomp', 'pomp_3_volume')
-pomp_4_volume = config.getint('pomp', 'pomp_4_volume')
+pump_1_volume = config.getint('pump', 'pump_1_volume')
+pump_2_volume = config.getint('pump', 'pump_2_volume')
+pump_3_volume = config.getint('pump', 'pump_3_volume')
+pump_4_volume = config.getint('pump', 'pump_4_volume')
 
 scheduledtime = config.get('time', 'time')
 
-pomp1insec = pomp_1_volume * mlpersec
-pomp2insec = pomp_2_volume * mlpersec
-pomp3insec = pomp_3_volume * mlpersec
-pomp4insec = pomp_4_volume * mlpersec
+pump1insec = pump_1_volume * mlpersec
+pump2insec = pump_2_volume * mlpersec
+pump3insec = pump_3_volume * mlpersec
+pump4insec = pump_4_volume * mlpersec
 
 #Tijd omzetten om te vergelijken
 scheduled = datetime.datetime.strptime(scheduledtime, "%H:%M")
 now = datetime.datetime.now()
 
-pomp1 = config.get('pomp', 'schedule_pomp_1')
-pomp2 = config.get('pomp', 'schedule_pomp_2')
-pomp3 = config.get('pomp', 'schedule_pomp_3')
-pomp4 = config.get('pomp', 'schedule_pomp_4')
+pump1 = config.get('pump', 'schedule_pump_1')
+pump2 = config.get('pump', 'schedule_pump_2')
+pump3 = config.get('pump', 'schedule_pump_3')
+pump4 = config.get('pump', 'schedule_pump_4')
 
 volume_available_1_ml = config.getint('volume_available', 'volume_available_1_ml')
 volume_available_2_ml = config.getint('volume_available', 'volume_available_2_ml')
@@ -50,18 +50,18 @@ volume_available_4_ml = config.getint('volume_available', 'volume_available_4_ml
 
 weekdag = date.today().weekday()
 
-if str(weekdag) in pomp1 and now.hour == scheduled.hour and now.minute == scheduled.minute:
-   	#Pomp1
+if str(weekdag) in pump1 and now.hour == scheduled.hour and now.minute == scheduled.minute:
+   	#pump1
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup(pomp_1_gpio,GPIO.OUT)
-	print "Pomp 1 aan voor %d sec om %d ml te pompen." % (pomp1insec,pomp_1_volume)
-	GPIO.output(pomp_1_gpio,GPIO.LOW)
-	time.sleep (pomp1insec)
-	print 'Pomp 1 uit.'
-	GPIO.output(pomp_1_gpio,GPIO.HIGH)
+	GPIO.setup(pump_1_gpio,GPIO.OUT)
+	print "pump 1 aan voor %d sec om %d ml te pumpen." % (pump1insec,pump_1_volume)
+	GPIO.output(pump_1_gpio,GPIO.LOW)
+	time.sleep (pump1insec)
+	print 'pump 1 uit.'
+	GPIO.output(pump_1_gpio,GPIO.HIGH)
 	GPIO.cleanup()
-	volume_available_1_ml_new = volume_available_1_ml - pomp_1_volume
+	volume_available_1_ml_new = volume_available_1_ml - pump_1_volume
 	volume_available_1 = 100 * volume_available_1_ml_new / reservoir_1
   	config = ConfigParser()
   	config.read('settings.ini')
@@ -71,20 +71,20 @@ if str(weekdag) in pomp1 and now.hour == scheduled.hour and now.minute == schedu
 		config.write(configfile)
 
 else :
-   	print ('Pomp 1 hoeft niet te pompen.')
+   	print ('pump 1 hoeft niet te pumpen.')
 
-if str(weekdag) in pomp2 and now.hour == scheduled.hour and now.minute == scheduled.minute:
-   	#Pomp2
+if str(weekdag) in pump2 and now.hour == scheduled.hour and now.minute == scheduled.minute:
+   	#pump2
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup(pomp_2_gpio,GPIO.OUT)
-	print "Pomp 2 aan voor %d sec om %d ml te pompen." % (pomp2insec,pomp_2_volume)
-	GPIO.output(pomp_2_gpio,GPIO.LOW)
-	time.sleep (pomp2insec)
-	print 'Pomp 2 uit.'
-	GPIO.output(pomp_2_gpio,GPIO.HIGH)
+	GPIO.setup(pump_2_gpio,GPIO.OUT)
+	print "pump 2 aan voor %d sec om %d ml te pumpen." % (pump2insec,pump_2_volume)
+	GPIO.output(pump_2_gpio,GPIO.LOW)
+	time.sleep (pump2insec)
+	print 'pump 2 uit.'
+	GPIO.output(pump_2_gpio,GPIO.HIGH)
 	GPIO.cleanup()
-	volume_available_2_ml_new = volume_available_2_ml - pomp_2_volume
+	volume_available_2_ml_new = volume_available_2_ml - pump_2_volume
 	volume_available_2 = 100 * volume_available_2_ml_new / reservoir_2
   	config = ConfigParser()
   	config.read('settings.ini')
@@ -93,20 +93,20 @@ if str(weekdag) in pomp2 and now.hour == scheduled.hour and now.minute == schedu
 	with open('settings.ini', 'w') as configfile:
 		config.write(configfile)
 else :
-   	print ('Pomp 2 hoeft niet te pompen.')
+   	print ('pump 2 hoeft niet te pumpen.')
 
-if str(weekdag) in pomp3 and now.hour == scheduled.hour and now.minute == scheduled.minute:
-   	#Pomp3
+if str(weekdag) in pump3 and now.hour == scheduled.hour and now.minute == scheduled.minute:
+   	#pump3
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup(pomp_3_gpio,GPIO.OUT)
-	print "Pomp 3 aan voor %d sec om %d ml te pompen." % (pomp3insec,pomp_3_volume)
-	GPIO.output(pomp_3_gpio,GPIO.LOW)
-	time.sleep (pomp3insec)
-	print 'Pomp 3 uit.'
-	GPIO.output(pomp_3_gpio,GPIO.HIGH)
+	GPIO.setup(pump_3_gpio,GPIO.OUT)
+	print "pump 3 aan voor %d sec om %d ml te pumpen." % (pump3insec,pump_3_volume)
+	GPIO.output(pump_3_gpio,GPIO.LOW)
+	time.sleep (pump3insec)
+	print 'pump 3 uit.'
+	GPIO.output(pump_3_gpio,GPIO.HIGH)
 	GPIO.cleanup()
-	volume_available_3_ml_new = volume_available_3_ml - pomp_3_volume
+	volume_available_3_ml_new = volume_available_3_ml - pump_3_volume
 	volume_available_3 = 100 * volume_available_3_ml_new / reservoir_3
   	config = ConfigParser()
   	config.read('settings.ini')
@@ -115,20 +115,20 @@ if str(weekdag) in pomp3 and now.hour == scheduled.hour and now.minute == schedu
 	with open('settings.ini', 'w') as configfile:
 		config.write(configfile)
 else :
-   	print ('Pomp 3 hoeft niet te pompen.')
+   	print ('pump 3 hoeft niet te pumpen.')
 
-if str(weekdag) in pomp4 and now.hour == scheduled.hour and now.minute == scheduled.minute:
-   	#Pomp4
+if str(weekdag) in pump4 and now.hour == scheduled.hour and now.minute == scheduled.minute:
+   	#pump4
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-	GPIO.setup(pomp_4_gpio,GPIO.OUT)
-	print "Pomp 4 aan voor %d sec om %d ml te pompen." % (pomp4insec,pomp_4_volume)
-	GPIO.output(pomp_4_gpio,GPIO.LOW)
-	time.sleep (pomp4insec)
-	print 'Pomp 4 uit.'
-	GPIO.output(pomp_4_gpio,GPIO.HIGH)
+	GPIO.setup(pump_4_gpio,GPIO.OUT)
+	print "pump 4 aan voor %d sec om %d ml te pumpen." % (pump4insec,pump_4_volume)
+	GPIO.output(pump_4_gpio,GPIO.LOW)
+	time.sleep (pump4insec)
+	print 'pump 4 uit.'
+	GPIO.output(pump_4_gpio,GPIO.HIGH)
 	GPIO.cleanup()
-	volume_available_4_ml_new = volume_available_4_ml - pomp_4_volume
+	volume_available_4_ml_new = volume_available_4_ml - pump_4_volume
 	volume_available_4 = 100 * volume_available_4_ml_new / reservoir_4
   	config = ConfigParser()
   	config.read('settings.ini')
@@ -137,4 +137,4 @@ if str(weekdag) in pomp4 and now.hour == scheduled.hour and now.minute == schedu
 	with open('settings.ini', 'w') as configfile:
 		config.write(configfile)
 else :
-   	print ('Pomp 4 hoeft niet te pompen.')
+   	print ('pump 4 hoeft niet te pumpen.')
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import time
 from configparser import ConfigParser
 
 gpio_pins = ['pump_1_gpio','pump_2_gpio', 'pump_3_gpio', 'pump_4_gpio']
-pump_names = ['macro voeding', 'micro voeding', 'ijzer']
+pump_names = ['alias_1', 'alias_2', 'alias_3', 'alias_4']
 
 def test_run(pump_number):
   pump_number = int(pump_number)
@@ -12,42 +12,18 @@ def test_run(pump_number):
   config.read('./settings.ini')
 
   flow_rate = config.getfloat('mlpersec', 'mlpersec')
-  pump_duration = 5 * flow_rate
+  pump_duration = 4 * flow_rate
 
   pump_pin = config.getint('gpio', gpio_pins[pump_number])
+  pump_alias = config.get('alias', pump_names[pump_number])
   GPIO.setmode(GPIO.BCM)
   GPIO.setwarnings(False)
   GPIO.setup(pump_pin,GPIO.OUT)
-  print"pump ",pump_names[pump_number]," aan voor ",pump_duration," sec om 5 ml te pumpen."
+  print"pump ", pump_alias," aan voor ",pump_duration," sec om 5 ml te pumpen."
   GPIO.output(pump_pin,GPIO.LOW)
   time.sleep (pump_duration)
-  print"pump ",pump_names[pump_number]," uit."
+  print"pump ", pump_alias," uit."
   GPIO.output(pump_pin,GPIO.HIGH)
-
-
-  pump_numbers = [0,1,2,3]
-
-  for pump_number in pump_numbers:
-    if pump_number == 0:
-      volume_available_1_ml_new = volume_available_1_ml - 5
-      volume_available = int(100 * int(volume_available_1_ml) / int(pump_1_volume_reservoir))
-      config.set('volume_available', 'volume_available_1_ml', str(volume_available_1_ml_new))
-      config.set('volume_available', 'volume_available_1', str(volume_available))
-
-    elif pump_number == 1:
-      volume_available = int(100 * int(volume_available_2_ml) / int(volume_reservoirs.pump_2_volume_reservoir))
-      config.set('volume_available', 'volume_available_2_ml', str(volume_available_ml.volume_available_2_ml))
-      config.set('volume_available', 'volume_available_2', str(volume_available))
-
-    elif pump_number == 2:
-      volume_available = int(100 * int(volume_available_3_ml) / int(volume_reservoirs.pump_3_volume_reservoir))
-      config.set('volume_available', 'volume_available_3_ml', str(volume_available_ml.volume_available_3_ml))
-      config.set('volume_available', 'volume_available_3', str(volume_available))
-
-    elif pump_number == 3:
-      volume_available = int(100 * int(volume_available_ml.volume_available_4_ml) / int(volume_reservoirs.pump_4_volume_reservoir))
-      config.set('volume_available', 'volume_available_4_ml', str(volume_available_ml.volume_available_4_ml))
-      config.set('volume_available', 'volume_available_4', str(volume_available))
 
 if __name__ == "__main__":
   pump_number = sys.argv[1]
